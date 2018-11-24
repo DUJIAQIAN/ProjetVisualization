b46b931096a2c06f59484e3bb04969770342c95a
DU JIAQIAN <jia.du@groupe-esigelec.org>
2018-11-24 21:04:16 +0100

    utiliser d3.neste pour treemap

--- traitement.js
+++ traitement.js
@@ -5,25 +5,30 @@ function traitement(data,mois,annee) {
         var objMois = {};
         for (var k = 0; k < mois.length; k++) {
             var dic = [];
-            for (var i = 1; i < data.length; i++) {
+            for (var i = 1; i < 2; i++) {
                 var item = data[i];
-                var itemAnnee = item[0].slice(0, 4);
-                var itemMois = item[0].slice(5, 7);
+                var itemAnnee = item.Annee;
+                var itemMois = item.Mois;
+//               console.log(item.Annee);
                 var objet = {
-                    "time": item[0],
-                    "gare": item[1],
-                    "nature": item[2],
-                    "type": item[3]
+                    "time": item.Annee,
+                    "gare": item.Gare,
+                    "nature": item.Nature,
+                    "type": item.Type
                 };
+//                 console.log(objet);
                 if (itemAnnee == annee[j] && itemMois == mois[k]) {
+                   
                     dic.push(objet);
                     objMois[itemMois] = dic;
+                     console.log(objMois)
                 }
             }
         }
 
-        donnees[annee[j]] = objMois;
+        //donnees[annee[j]] = objMois;
+       // console.log(donnees);
     }
 
-    return donnees;
+//    return donnees;
 }
